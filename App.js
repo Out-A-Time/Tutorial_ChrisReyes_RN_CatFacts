227907f7ee0e18969c6d9827aef6ac24290e8c14
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2024-07-10 19:03:53 +0100

    Fetching data from API

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import {
+ StyleSheet,
+ Text,
+ View,
+ FlatList,
+ TouchableOpacity,
+ SafeAreaView,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+const requestOptions = {
+ method: "GET",
+};
 
 export default function App() {
+ const [facts, setFacts] = useState();
+
+ useEffect(() => {
+  const fetchData = async () => {
+   try {
+    const res = await fetch(
+     "https://cat-fact.herokuapp.com/#/cat/facts",
+     requestOptions
+    );
+    const json = await res.json();
+    setFacts([...json?.all]);
+   } catch (error) {
+    console.log(error);
+    setFacts(error);
+   }
+  };
+
+  fetchData();
+ }, []);
+
+ function Item({ title }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <TouchableOpacity
+    onPress={() => {
+     console.log("button pressed");
+    }}
+   >
+    <View style={styles.item}>
+     <Text style={styles.item}>{title}</Text>
     </View>
+   </TouchableOpacity>
   );
+ }
+
+ return (
+  <SafeAreaView style={styles.container}>
+   <FlatList
+    data={facts}
+    renderItem={(fact) => <Item title={fact?.item?.text} />}
+    keyExtractor={(item, i) => `${item.id}${i}`}
+   />
+  </SafeAreaView>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+  flex: 1,
+  backgroundColor: "#fff",
+  alignItems: "center",
+  justifyContent: "center",
+ },
+ item: {
+  color: "black",
+  padding: 20,
+  marginVertical: 8,
+  marginHorizontal: 16,
+ },
+ title: {
+  fontSize: 32,
+ },
 });
